463a15e09410254c27cf653428306fcb040619d2
wdog <wdog666@gmail.com>
2025-05-25 22:07:29 +0200

    filament + shield installer

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/bash
 
-composer='docker run --rm -it --user 1000:1000 -v .:/opt -w /opt composer:latest'
+set -e
+
+#sudo rm -rf ~/.cache/composer
+mkdir -p ~/.cache/composer
+#chmod 777 ~/.cache/composer -R
+
+composer="docker run --rm -it --user 1000:1000 -v .:/opt -v $HOME/.cache/composer:/tmp/cache -w /opt composer:latest"
+run_in_app="docker-compose exec -u 1000:1000 app"
 
 # Variabili di configurazione
 PROJECT_DIR="src"
@@ -10,23 +17,45 @@ DB_PASS="laravel"
 APP_NAME="MyLaravelApp"
 
 
-createDir(){
+main(){
+    createDir
+    setupProject
+    keyGenerate
+    installFilament
+    startUpDocker
+    install_npm
+    run_migrations
+    vite
+    setupFilament
+    installShield
+    createUser
+    setupShield
+    banner "Installazione completata!"
+    # echo "Accesso all'applicazione: https://localhost"
+    # echo "Utente di test creato:"
+    # echo "Email: admin@example.com"
+    # echo "Password: password"
+}
 
-    docker-compose down --remove-orphans
 
+
+createDir() {
     banner "Clean Existing Directories"
     sudo rm -rf mysql
     rm -rf "$PROJECT_DIR"
 
     banner "Creazione della directory src"
     mkdir -p "$PROJECT_DIR"
-    cd "$PROJECT_DIR" || { echo "Impossibile accedere alla directory $PROJECT_DIR"; exit 1; }
+    cd "$PROJECT_DIR" || {
+        echo "Impossibile accedere alla directory $PROJECT_DIR"
+        exit 1
+    }
 
     banner "Installazione di Laravel..."
-    $composer create-project --prefer-dist laravel/laravel . --no-scripts --quiet
+    $composer create-project --prefer-dist laravel/laravel . --no-scripts
 }
 
-setupProject(){
+setupProject() {
     # Configurazione del file .env
     banner "Configurazione del file .env..."
     cp .env.example .env
@@ -45,29 +74,27 @@ setupProject(){
     sed -i "s/# DB_PASSWORD=.*/DB_PASSWORD=$DB_PASS/" .env
 }
 
-
-keyGenerate(){
+keyGenerate() {
     $composer php artisan key:generate
     git init
     git add .
     git commit -a -m "inital commit"
 }
 
-installFilament(){
+installFilament() {
     banner "Installazione di Filament..."
-    $composer require filament/filament  --ignore-platform-reqs -W --quiet
+    $composer require filament/filament --ignore-platform-reqs -W
 }
 
-installShield(){
+installShield() {
     banner "Installazione di Filament Shield..."
-    $composer require bezhansalleh/filament-shield --ignore-platform-reqs -W --quiet
-    $composer php artisan vendor:publish --tag=filament-shield
+    $composer require bezhansalleh/filament-shield --ignore-platform-reqs -W
 }
 
-createUser(){
-# Creazione di un utente di test tramite seeder
-banner "Creazione di un utente admin..."
-cat <<EOL > database/seeders/UserSeeder.php
+createUser() {
+    # Creazione di un utente di test tramite seeder
+    banner "Creazione di un utente admin..."
+    cat <<EOL >database/seeders/UserSeeder.php
 <?php
 
 namespace Database\Seeders;
@@ -88,47 +115,130 @@ class UserSeeder extends Seeder
 }
 EOL
 
-# Aggiornamento del file DatabaseSeeder.php
-sed -i "s/class DatabaseSeeder extends Seeder/class DatabaseSeeder extends Seeder\n{\n    public function run()\n    {\n        \$this->call(UserSeeder::class);\n    }\n}/" database/seeders/DatabaseSeeder.php
+    # Aggiornamento del file DatabaseSeeder.php
+    cat <<EOL >database/seeders/DatabaseSeeder.php
+<?php
 
+namespace Database\Seeders;
 
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+        \$this->call(UserSeeder::class);
+    }
+}
+EOL
+
+    $run_in_app php artisan db:seed
 }
 
+startUpDocker() {
 
-startUp(){
-
+    banner "Avvio dell'applicazione..."
+    docker-compose down --remove-orphans
     docker-compose up -d
-    docker-composer exec -u 1000:1000 app npm install
-    sleep 3
-    $composer php artisan migrate --step --seed
-}
-
-setupShield(){
-    $compose php artisan shield:install
-    $compose php artisan shield:setup
+    sleep 5
 }
 
 
-banner(){
-    echo "----- " $1 " -----"
+install_npm(){
+    banner "Installazione delle dipendenze NODEJS..."
+    $run_in_app npm install
+}
+
+
+vite(){
+
+    cat <<EOL >vite.config.js
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import laravel, { refreshPaths } from 'laravel-vite-plugin'
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: [
+                ...refreshPaths,
+                'app/Filament/**',
+            ],
+        }),
+        tailwindcss(),
+    ],
+    server: {
+        host: "0.0.0.0",
+        hmr: {
+            host: "localhost",
+            https: true
+        }
+    },
+});
+EOL
+
+}
+
+setupFilament(){
+    $run_in_app php artisan filament:install -F -n --panels
+
+    sed -i "s/->path('admin')/->path('\/')/" app/Providers/Filament/AdminPanelProvider.php
+    echo "<?php" > routes/web.php
+
+    cat << EOL > app/Models/User.php
+<?php
+
+namespace App\Models;
+
+use Filament\Panel;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable implements FilamentUser
+{
+    use HasFactory, Notifiable, HasRoles;
+
+    public function canAccessPanel(Panel \$panel): bool
+    {
+        return true;
+    }
+}
+
+EOL
+
+
+}
+
+setupShield() {
+    banner "setup shield..."
+    $run_in_app php artisan vendor:publish --tag=filament-shield-config
+    $run_in_app php artisan vendor:publish --tag=filament-shield-translations
+    $run_in_app php artisan shield:setup --fresh --minimal
+    $run_in_app php artisan shield:install admin -n
+    $run_in_app php artisan shield:generate --panel=admin --all --ignore-existing-policies
+    $run_in_app php artisan shield:super-admin
+
+}
+
+
+run_migrations(){
+    banner "Esecuzione delle migrations e seeder..."
+    $run_in_app php artisan migrate --step
+}
+
+banner() {
+    echo "--------------------------------------------------------------"
+    echo ">>>>>" $1
+    echo "--------------------------------------------------------------"
+    echo ""
 }
 
 
 
-#createDir
-# setupProject
- keyGenerate
-# installFilament
-# installShield
-# setupShield
-# createUser
-# startUp
-
-# Esecuzione del seeder
-#php artisan db:seed
-
-echo "Installazione completata!"
-echo "Accesso all'applicazione: https://localhost"
-echo "Utente di test creato:"
-echo "Email: admin@example.com"
-echo "Password: password"
+# RUN
+main
